7ae9567452d3f1b4b5eff6f2dc07bcfac8656004
Cinilope <69024442+Cinilope@users.noreply.github.com>
2021-10-31 17:03:00 -0500

    created sign, wave, and selection blank screens and added button navigation from home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 import 'package:flutter/material.dart';
+import 'package:googleapis/drive/v3.dart' as drive;
+import 'package:google_sign_in/google_sign_in.dart' as signIn;
+
+import 'GoogleAuthClient.dart';
+import 'dart:io';
+import 'package:pdf/widgets.dart' as pw;
 
 void main() {
   runApp(MyApp());
@@ -28,7 +35,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key key, title, this.appTitle}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -39,7 +46,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String appTitle;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -48,10 +55,25 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _state = "Welcome Screen";
 
- void _MakeState(String inputState) {
-    setState(() {
-      _state =  inputState;
-    });
+  Future<void> _MakeState(String inputState) async {
+    setState(() {});
+    /*final googleSignIn = signIn.GoogleSignIn.standard(
+        scopes: [drive.DriveApi.driveScope]);
+    final signIn.GoogleSignInAccount account = (await googleSignIn.signIn());
+    print("User account $account");
+
+
+    final authHeaders = await account.authHeaders;
+    final authenticateClient = GoogleAuthClient(authHeaders);
+    final driveApi = drive.DriveApi(authenticateClient);
+
+    final Stream<List<int>> mediaStream = Future.value([104, 105]).asStream();
+    var media = new drive.Media(mediaStream, 2);
+    var driveFile = new drive.File();
+    driveFile.name = "hello_world.txt";
+    final result = await driveApi.files.create(driveFile, uploadMedia: media);
+    print("Upload result: $result");
+    PDFDocument doc = await PDFDocument.fromAsset('assets/pdfs/McCutcheon-Critics Not Caretakers ch. 8.pdf');*/
   }
 
   @override
@@ -63,12 +85,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -85,55 +105,62 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-           Container(
-             child: Text(_state,
-               textScaleFactor: 2,
-                 textAlign: TextAlign.center,
-                 style: TextStyle(fontWeight: FontWeight.bold)
-      ),
+            Image.asset('assets/images/logo anim1.gif'),
+            /*Row(
+              children: <Widget>[
+                Text(
+                  'Welcome to the \n document signing solution',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontSize: 30,
+                    foreground: Paint()
+                      ..style = PaintingStyle.fill
+                      ..strokeWidth = 2
+                      ..color = Colors.blue
+                  ),
+                ),
+                // Solid text as fill.
 
-             color: Colors.blue,
-               padding:EdgeInsets.all(20.0),
-             margin: EdgeInsets.all(20.00),
-
-           ),
-            Image.asset('assets/images/Awavbaklogo.jpeg'),
+              ],
+            ),*/
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 ElevatedButton(
                   onPressed: () {
+                    _MakeState("hello");
                     Navigator.push(
-                      context,new MaterialPageRoute(builder: (context) => new signScreen()),
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => new signScreen()),
                     );
                   },
-
                   child: Container(
                     alignment: Alignment.bottomLeft,
                     child: Text("Signer"),
                     color: Colors.blue,
-                    padding:EdgeInsets.all(30.0),
+                    padding: EdgeInsets.all(30.0),
                     margin: EdgeInsets.all(20.00),
-
                   ),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.push(
-                      context,new MaterialPageRoute(builder: (context) => new waverScreen()),
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => new waverScreen()),
                     );
                   },
                   child: Container(
                     alignment: Alignment.bottomRight,
                     child: Text("Waver"),
                     color: Colors.blue,
-                   padding:EdgeInsets.all(30.0),
-                   margin: EdgeInsets.all(20.00),
+                    padding: EdgeInsets.all(30.0),
+                    margin: EdgeInsets.all(20.00),
                   ),
                 ),
               ],
             )
-
           ],
         ),
       ),
@@ -145,97 +172,315 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class waverScreen extends StatelessWidget {
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
+    var ui;
     return new Scaffold(
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/lines.gif'),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+             Row(
+                children: <Widget>[
+                  Image.asset('assets/images/document icon.png',width: 200,height: 200, alignment: Alignment.centerLeft,),
+                  Image.asset('assets/images/pen icon.png',width: 200,height: 200, alignment: Alignment.center,),
+                  //Image.asset('assets/images/form icon.png',width: 200,height: 200, alignment: Alignment.centerRight,),
 
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            //Image.asset('assets/images/Awavbaklogo.jpeg'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,new MaterialPageRoute(builder: (context) => new selectionScreen()),
-                    );
-                  },
-                  child: Container(
-                    alignment: Alignment.bottomLeft,
-                    child: Text("Select"),
-                    color: Colors.blue,
-                    padding:EdgeInsets.all(30.0),
-                    margin: EdgeInsets.all(20.00),
-
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => new SelectListItem()),
+                      );
+                    },
+                    child: Container(
+                      alignment: Alignment.bottomLeft,
+                      child: Text("Select"),
+                      color: Colors.blue,
+                      padding: EdgeInsets.all(30.0),
+                      margin: EdgeInsets.all(20.00),
+                    ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,new MaterialPageRoute(builder: (context) => new selectionScreen()),
-                    );
-                  },
-                  child: Container(
-                    alignment: Alignment.bottomRight,
-                    child: Text("Create"),
-                    color: Colors.blue,
-                    padding:EdgeInsets.all(30.0),
-                    margin: EdgeInsets.all(20.00),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => new creationScreen()),
+                      );
+                    },
+                    child: Container(
+                      alignment: Alignment.bottomRight,
+                      child: Text("Create"),
+                      color: Colors.blue,
+                      padding: EdgeInsets.all(30.0),
+                      margin: EdgeInsets.all(20.00),
+                    ),
                   ),
-                ),
-              ],
-            )
-
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class selectionScreen extends StatelessWidget {
+class SelectListItem extends StatefulWidget {
   @override
-  Widget build (BuildContext context){
-    return Scaffold(
+  _SelectListItemState createState() => _SelectListItemState();
+}
 
+class _SelectListItemState extends State<SelectListItem> {
+  List<String> _selectedItems = List<String>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/document icon.png',
+                fit: BoxFit.contain,
+                height: 32,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8.0), child: Text('Select Waver'))
+            ],
+
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index) {
+            return Container(
+              color: (_selectedItems.contains(index))
+                  ? Colors.blue.withOpacity(0.5)
+                  : Colors.transparent,
+              child: ListTile(
+                onTap: () {
+                  if (_selectedItems.contains(index)) {
+                    setState(() {
+                      _selectedItems.removeWhere((val) => val == index);
+                    });
+                  }
+                },
+                onLongPress: () {
+                  if (!_selectedItems.contains(index)) {
+                    setState(() {
+                      _selectedItems.add(' Waver title: $index');
+                    });
+                  }
+                },
+                title: Text('Waver title: $index'),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
 
-class creationScreen extends StatelessWidget {
+class creationScreen extends StatefulWidget {
   @override
-  Widget build (BuildContext context){
-    return Scaffold(
+  _creationScreenState createState() => _creationScreenState();
+}
 
-    );
+class _creationScreenState extends State<creationScreen> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/pen icon.png',
+                fit: BoxFit.contain,
+                height: 32,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8.0), child: Text('Create a PDF'))
+            ],
+
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Center(
+            child: TextField(
+              keyboardType: TextInputType.text,
+              maxLines: 3,
+              controller: myController,
+            ),
+          ),
+        ));
   }
 }
 
-class signScreen extends StatelessWidget{
-  @override
-  Widget build (BuildContext context){
-    return Scaffold(
+class signScreen extends StatefulWidget {
+  const signScreen({Key key}) : super(key: key);
 
-    );
+  @override
+  _signScreenWidgetState createState() {
+    return _signScreenWidgetState();
   }
 }
 
+class _signScreenWidgetState extends State<signScreen> {
+  bool _isLoading = true;
+  PDFDocument document;
+
+  @override
+  void initState() {
+    super.initState();
+    loadDocument();
+  }
+
+  loadDocument() async {
+    document = await PDFDocument.fromAsset('assets/sample.pdf');
+
+    setState(() => _isLoading = false);
+  }
+
+  changePDF(value) async {
+    setState(() => _isLoading = true);
+    if (value == 1) {
+      document = await PDFDocument.fromAsset('assets/sample2.pdf');
+    } else if (value == 2) {
+      document = await PDFDocument.fromURL(
+        "http://conorlastowka.com/book/CitationNeededBook-Sample.pdf",
+        /* cacheManager: CacheManager(
+          Config(
+            "customCacheKey",
+            stalePeriod: const Duration(days: 2),
+            maxNrOfCacheObjects: 10,
+          ),
+        ), */
+      );
+    } else {
+      document = await PDFDocument.fromAsset('assets/sample.pdf');
+    }
+    setState(() => _isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 36),
+              ListTile(
+                title: Text('Load from Assets'),
+                onTap: () {
+                  changePDF(1);
+                },
+              ),
+              ListTile(
+                title: Text('Load from URL'),
+                onTap: () {
+                  changePDF(2);
+                },
+              ),
+              ListTile(
+                title: Text('Restore default'),
+                onTap: () {
+                  changePDF(3);
+                },
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/form icon.png',
+                fit: BoxFit.contain,
+                height: 32,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8.0), child: Text('Sign Here'))
+            ],
+
+          ),
+        ),
+        body: Center(
+          child: _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : PDFViewer(
+                  document: document,
+                  zoomSteps: 1,
+                  //uncomment below line to preload all pages
+                  // lazyLoad: false,
+                  // uncomment below line to scroll vertically
+                  // scrollDirection: Axis.vertical,
+
+                  //uncomment below code to replace bottom navigation with your own
+                  /* navigationBuilder:
+                      (context, page, totalPages, jumpToPage, animateToPage) {
+                    return ButtonBar(
+                      alignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.first_page),
+                          onPressed: () {
+                            jumpToPage()(page: 0);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () {
+                            animateToPage(page: page - 2);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.arrow_forward),
+                          onPressed: () {
+                            animateToPage(page: page);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.last_page),
+                          onPressed: () {
+                            jumpToPage(page: totalPages - 1);
+                          },
+                        ),
+                      ],
+                    );
+                  }, */
+                ),
+        ),
+      ),
+    );
+  }
+}
